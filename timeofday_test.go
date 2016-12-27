1e5bdeb4a72bdc9d7202e24212967a733a315f32
Jurgen <jurgen.kluft@gmail.com>
2016-12-27 18:07:03 +0800

    Update timeofday_test.go

--- timeofday_test.go
+++ timeofday_test.go
@@ -12,42 +12,42 @@ func TestTimeOfDayParsing(t *testing.T) {
 		c.Convey("When all digits are set", func() {
 			clock := ParseTimeOfDay("12:13:14")
 			c.Convey("The timeofday should be 12:13:14", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 12, Minutes: 13, Seconds: 14})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 12, Minutes: 13, Seconds: 14})
 			})
 		})
 
 		c.Convey("When single digits are set", func() {
 			clock := ParseTimeOfDay("2:3:4")
 			c.Convey("The timeofday should be 2:3:4", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
 			})
 		})
 
 		c.Convey("Testing AM", func() {
 			clock := ParseTimeOfDay("2:3:4 AM")
 			c.Convey("The timeofday should be 2:3:4", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
 			})
 		})
 
 		c.Convey("Testing AM correction", func() {
 			clock := ParseTimeOfDay("14:3:4 AM")
 			c.Convey("The timeofday should be 2:3:4", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
 			})
 		})
 
 		c.Convey("Testing PM", func() {
 			clock := ParseTimeOfDay("14:3:4 PM")
 			c.Convey("The timeofday should be 14:3:4", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 14, Minutes: 3, Seconds: 4})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 14, Minutes: 3, Seconds: 4})
 			})
 		})
 
 		c.Convey("Testing PM correction", func() {
 			clock := ParseTimeOfDay("2:3:4 PM")
 			c.Convey("The timeofday should be 14:3:4", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 14, Minutes: 3, Seconds: 4})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 14, Minutes: 3, Seconds: 4})
 			})
 		})
 
@@ -56,7 +56,7 @@ func TestTimeOfDayParsing(t *testing.T) {
 			clock2 := ParseTimeOfDay("0:3:4")
 			clock.Add(clock2)
 			c.Convey("The result should be 2:3:4", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
 			})
 		})
 
@@ -65,21 +65,21 @@ func TestTimeOfDayParsing(t *testing.T) {
 			clock2 := ParseTimeOfDay("7:5:9")
 			clock.Add(clock2)
 			c.Convey("The result should be 2:3:4", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 2, Minutes: 3, Seconds: 4})
 			})
 		})
 		
 		c.Convey("Testing HH:MM, mod", func() {
 			clock := ParseTimeOfDay("18:57")
 			c.Convey("The result should be 18:57:00", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 18, Minutes: 57, Seconds: 0})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 18, Minutes: 57, Seconds: 0})
 			})
 		})
 		
 		c.Convey("Testing HH:MM PM, mod", func() {
 			clock := ParseTimeOfDay("18:57 PM")
 			c.Convey("The result should be 18:57:00", func() {
-				c.So(clock, c.ShouldResemble, TimeOfDay{Hours: 18, Minutes: 57, Seconds: 0})
+				c.So(clock, c.ShouldResemble, &TimeOfDay{Hours: 18, Minutes: 57, Seconds: 0})
 			})
 		})
 
